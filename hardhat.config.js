56a476b945721a0363598b06ff7c6710a0853c22
Gulshan Vasnani <gulshanslvasnani@gmail.com>
2021-08-04 14:28:49 +0530

    Added hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,78 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const INFURA_API = process.env.INFURA_API;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.apiKey
+  },
+  networks: {
+    localhost: {
+      url: "http://localhost:8545", // uses account 0 of the hardhat node to deploy
+    },
+    polygon: {
+      url: INFURA_API,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    matic: {
+      url: INFURA_API,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    hardhat: {
+      forking: {
+        url: INFURA_API,
+      },
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.7.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+}
